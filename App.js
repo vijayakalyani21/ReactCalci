8acc6649b016b39292e0523bccecc0fddd795913
vijayakalyani21 <137884380+vijayakalyani21@users.noreply.github.com>
2023-08-11 02:56:46 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Calc from './components/Calc';
+function App() {
+    return (
+      <div className="App">
+     <Calc/>
+   </div>
+    );
+  }
+  
+  export default App;
